ef234059989441ce37ecf9663f0f17634e63b11f
Tuan12342 <tuanhanam120@gmail.com>
2024-12-22 08:19:42 +0700

    Delete OOP_20225679_Lab5 directory

--- OOP_20225679_Lab5/AimsProject/src/hust/soict/ite6/aims/exception/PlayerException.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.ite6.aims.exception;
-
-public class PlayerException extends Exception {
-    public PlayerException(String message) {
-        super(message);
-    }
-}
--- OOP_20225679_Lab5/AimsProject/src/hust/soict/ite6/aims/media/Playable.java
+++
@@ -1,6 +0,0 @@
-package hust.soict.ite6.aims.media;
-public interface Playable {
-	
-	public void play();
-	
-}
