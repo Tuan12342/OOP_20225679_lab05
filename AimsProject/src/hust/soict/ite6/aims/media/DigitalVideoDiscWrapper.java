6089ea07cd680c053d4f12b9f608db358c3f19b8
Tuan12342 <tuanhanam120@gmail.com>
2024-12-22 09:23:46 +0800

    Add files via upload

---
+++ AimsProject/src/hust/soict/ite6/aims/media/DigitalVideoDiscWrapper.java
@@ -0,0 +1,12 @@
+package hust.soict.ite6.test.media;
+import hust.soict.ite6.aims.media.DigitalVideoDisc;
+
+public class DigitalVideoDiscWrapper {
+	DigitalVideoDisc dvd;
+	
+	DigitalVideoDiscWrapper(DigitalVideoDisc dvd)
+	{
+		super();
+		this.dvd = dvd;
+	}
+}
